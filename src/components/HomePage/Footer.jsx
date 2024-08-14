8495ce00c203ebf01072cee8b2724c9981ebfeef
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-08-14 09:13:15 +0600

    navbar done

---
+++ src/components/HomePage/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='bg-blackish text-gray-500 text-center py-4 pb-16 mdamaaamemde3daemdam'>Footer</div>
+  )
+}
